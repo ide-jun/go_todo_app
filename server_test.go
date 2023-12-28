471f170c2afda9965c39162c894bdc9945612394
ide-jun <IDEdevelop16@gmail.com>
2023-12-29 01:28:34 +0900

    define server

--- server_test.go
+++ server_test.go
@@ -13,8 +13,6 @@ import (
 )
 
 func TestRun(t *testing.T) {
-	t.Skip("refactoring")
-
 	l, err := net.Listen("tcp", "localhost:0")
 	if err != nil {
 		t.Fatalf("failed to listen port %v\n", err)
@@ -22,8 +20,13 @@ func TestRun(t *testing.T) {
 
 	ctx, cancel := context.WithCancel(context.Background())
 	eg, ctx := errgroup.WithContext(ctx)
+	mux := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %s!\n", r.URL.Path[1:])
+	})
+
 	eg.Go(func() error {
-		return run(ctx)
+		s := NewServer(l, mux)
+		return s.Run(ctx)
 	})
 
 	in := "message"
